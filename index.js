2f7bd6c88c10bfdd22ff5d90c88893fc2932038f
Neeraj Reddy Avutu <neerajreddyavutu@Neerajs-MacBook-Pro.local>
2020-07-26 20:47:19 +0530

    use path

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+// app.use(express.static(__dirname));
+app.use(express.static(__dirname+'/frontend'));
+
 app.get('/',function(req,res) {
 	// res.send("Hello World");
 	res.sendFile(__dirname+'/frontend/html/cover_letter.html');
