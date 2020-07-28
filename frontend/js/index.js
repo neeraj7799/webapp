1e2f8d7cf232744210c83fc51eabb8ba02471c4a
Neeraj Reddy Avutu <neerajreddyavutu@Neerajs-MacBook-Pro.local>
2020-07-28 11:05:21 +0530

    draw numbers

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -6,14 +6,15 @@ ctx.translate(radius, radius);
 console.log(radius);
 radius = radius * 0.90
 drawClock();
-function drawClock() {
-    ctx.arc(0, 0, radius, 0 , 2 * Math.PI);
-    ctx.fillStyle = "white";
-    ctx.fill();
-}
+// function drawClock() {
+//     ctx.arc(0, 0, radius, 0 , 2 * Math.PI);
+//     ctx.fillStyle = "white";
+//     ctx.fill();
+// }
 
 function drawClock() {
     drawFace(ctx, radius);
+    drawNumbers(ctx, radius);
   }
   
   function drawFace(ctx, radius) {
@@ -36,4 +37,21 @@ function drawClock() {
     ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
     ctx.fillStyle = '#333';
     ctx.fill();
+  }
+  function drawNumbers(ctx, radius) {
+    var ang;
+    var num;
+    ctx.font = radius * 0.15 + "px arial";
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
+    for(num = 1; num < 13; num++){
+      ang = num * Math.PI / 6;
+      ctx.rotate(ang);
+      ctx.translate(0, -radius * 0.85);
+      ctx.rotate(-ang);
+      ctx.fillText(num.toString(), 0, 0);
+      ctx.rotate(ang);
+      ctx.translate(0, radius * 0.85);
+      ctx.rotate(-ang);
+    }
   }
