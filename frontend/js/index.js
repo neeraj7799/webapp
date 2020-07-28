e8fba006db8453eab88dfbbd2e1bfbe49096d452
Neeraj Reddy Avutu <neerajreddyavutu@Neerajs-MacBook-Pro.local>
2020-07-28 10:55:41 +0530

    log radius

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,7 +1,9 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var radius = canvas.height / 2;
+console.log(radius);
 ctx.translate(radius, radius);
+console.log(radius);
 radius = radius * 0.90
 drawClock();
 function drawClock() {
