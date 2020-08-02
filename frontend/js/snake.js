172985aaf3f3cca0deb2f4b6a9a588243044a870
Neeraj Reddy Avutu <neerajreddyavutu@Neerajs-MacBook-Pro.local>
2020-08-02 20:23:04 +0530

    test2

--- frontend/js/snake.js
+++ frontend/js/snake.js
@@ -16,7 +16,20 @@ var snake;
 }());
 
 
-
+function drawSnake(){
+    this.x=0;
+    this.y=0;
+    this.xspeed= scale=1;
+    this.yspeed=0;
+    this.draw=function() {
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fillRect(this.x,this.y,scale,scale);
+    }
+    this.update=function() {
+        this.x+=this.xspeed;
+        this.y+=this.yspeed;
+    }
+}
 
 // const { text } = require("express");
 
