5834975be06c67fad16ba9ae6b4f1f9f338ab71e
Neeraj Reddy Avutu <neerajreddyavutu@Neerajs-MacBook-Pro.local>
2020-08-02 20:25:53 +0530

    ff

--- frontend/js/snake.js
+++ frontend/js/snake.js
@@ -19,7 +19,7 @@ var snake;
 function drawSnake(){
     this.x=0;
     this.y=0;
-    this.xspeed= scale=1;
+    this.xspeed= scale*1;
     this.yspeed=0;
     this.draw=function() {
         ctx.fillStyle = "#FFFFFF";
