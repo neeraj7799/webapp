4f7816740745400ee2764d2fa7656066b14e966c
Neeraj Reddy Avutu <neerajreddyavutu@Neerajs-MacBook-Pro.local>
2020-08-16 19:45:40 +0530

    housie

--- frontend/js/housie.js
+++ frontend/js/housie.js
@@ -1,17 +1,43 @@
-
-$('#h1Heading').html('New Heading')
-
-$('#btnGetData').click(function(){
-    $.ajax({
-        url : '/api/students',
-        method: 'GET',
-        success : function(res){
-            // TODO:  Using JQUERY SYNTAX
-            // Add this data as a table to your page
-            console.log("RES: " + JSON.stringify(res));
-        },
-        error: function(err){
-            console.log("ERR: "+err);
+$(function(){
+    var prev;
+    var min=1;
+    var max=90;
+    var count=0;
+    var number=[];
+    var random;
+    $("button").click(function(){
+        prev=random;
+        random = Math.floor(Math.random() * (max-min+1) )+min;
+        random++;
+        if(count==90){
+            alert("Limit Exhausted");
         }
-    })    
+        else{
+            while(number[random]=="true"){
+                random = Math.floor(Math.random() * (max-min+1))+min;
+
+            }
+                number[random]="true";
+            count++;
+            if(count>1){
+                
+                 $(".previous").html(prev)         
+        }
+        
+            $(".number").html(random)
+         var a=$("td");
+        var c=0;
+      a.each(function(){
+        if(c==random){
+             random= random.toString();
+              console.log(typeof(random));
+             
+              $(this).html("<span>"+random.toString()+"</span>");    
+          }
+          c++;
+      })
+
+        }
+
+    })
 })
